f951588443ed9a79250622516a1827e758f3ec1c
thedevyash <imailsharma.yash@gmail.com>
2025-08-25 11:54:11 +0530

    error-banner-fix

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@
 
 import holdings from "@/data/holdings.json";
 import PortfolioTable from "@/components/PortfolioTable";
+import ErrorBanner from "@/components/ErrorBanner";
 import { usePortfolioData } from "@/hooks/usePortfolioData";
 
 export default function Home() {
